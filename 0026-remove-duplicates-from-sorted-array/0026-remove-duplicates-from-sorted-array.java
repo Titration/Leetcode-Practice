5647ff519a2588d742dd868aab4aa89cf5caa79a
SX <shanxue2019@gmail.com>
2023-05-18 13:35:30 -0400

    Time: 1 ms (99.91%), Space: 44.2 MB (54.82%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,17 +1,14 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
         
-        int cout_dup = 0;
         int k = 1;
         
         for (int i=0; i<nums.length; i++) {
             if (i+1 < nums.length && nums[i] == nums[i+1]) {
-                cout_dup++;
             }
             else if (i+1 < nums.length && nums[i] != nums[i+1]) {
                 nums[k] = nums[i+1];
                 k++;
-                cout_dup = 0;
                 continue;
             }
         }
