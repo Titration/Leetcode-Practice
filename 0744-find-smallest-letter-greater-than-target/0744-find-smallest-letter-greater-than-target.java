6c001d7b9ba4fb0d264eaf764f4614a9072b816b
SX <shanxue2019@gmail.com>
2023-05-22 18:23:54 -0400

    Time: 0 ms (100.00%), Space: 44.7 MB (5.25%) - LeetHub

--- 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.java
+++ 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.java
@@ -1,21 +1,20 @@
 class Solution {
     public char nextGreatestLetter(char[] letters, char target) {
-        
+        // binary search
         int l = 0, r = letters.length - 1;
+        
+        if (target >= letters[r]) {
+            return letters[0];
+        }
+        
         while (l <= r) {
             int mid = l + (r-l) / 2;
             if (letters[mid] > target) {
                 r = mid - 1;
             }
-            else if (letters[mid] < target) {
+            else if (letters[mid] <= target) {
                 l = mid + 1;
             }
-            else {
-                l = mid + 1;
-            }
-        }
-        if (l > letters.length - 1) {
-            return letters[0];
         }
         return letters[l];
     }
