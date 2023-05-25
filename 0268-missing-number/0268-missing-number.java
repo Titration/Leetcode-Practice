7e0600a0474d709151ad878c7467537339cb1110
SX <shanxue2019@gmail.com>
2023-05-25 15:31:00 -0400

    Time: 6 ms (28.92%), Space: 44 MB (7.05%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,13 +1,18 @@
 class Solution {
     public int missingNumber(int[] nums) {
         
-        //sort + linear search. O(n)
+        //sort + binary search. O(logn)
         Arrays.sort(nums);
-        for (int i=0; i<nums.length; i++) {
-            if (nums[i] != i) {
-                return i;
+        int l = 0, r = nums.length - 1;
+        while(l <= r) {
+            int mid = l + (r-l)/2;
+            if (nums[mid] > mid) {
+                r = mid - 1;
+            }
+            else {
+                l = mid + 1;
             }
         }
-        return nums.length;
+        return l;
     }
 }
