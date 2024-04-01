8abb6ed07e98db69f7b2b194979ca394d3e1f086
SX <shanxue2019@gmail.com>
2024-03-31 21:55:40 -0400

    Time: 11 ms (74.99%), Space: 58.1 MB (59.89%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,13 +1,27 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        Arrays.sort(nums);
-        int p = nums[0];
-        for (int i = 1; i < nums.length; i++) {
-            if (p == nums[i]) {
+        /* Approach 1:
+        // Arrays.sort(nums);
+        // int p = nums[0];
+        // for (int i = 1; i < nums.length; i++) {
+        //     if (p == nums[i]) {
+        //         return true;
+        //     } else {
+        //         p = nums[i];
+        //     }
+        // }
+        // return false;
+        */
+        
+        /* Approach 2:
+        
+        */
+        HashSet<Integer> nums_hashset = new HashSet<>();
+        for (int num : nums) {
+            if (nums_hashset.contains(num)) {
                 return true;
-            } else {
-                p = nums[i];
             }
+            nums_hashset.add(num);
         }
         return false;
     }
