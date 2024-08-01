11a5ffc2a1e18e7f01387d420ace786f78908dc0
SX <shanxue2019@gmail.com>
2024-07-31 20:50:57 -0400

    Time: 19 ms (46.24%), Space: 45.3 MB (33.58%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -4,20 +4,17 @@ class Solution {
         if (s == null || t == null || s.length() < t.length()) {
             return "";
         }
-        
+
         HashMap<Character, Integer> window = new HashMap<>();
         HashMap<Character, Integer> t_map = new HashMap<>();
+
         for (char letter : t.toCharArray()) {
             t_map.put(letter, t_map.getOrDefault(letter, 0) + 1);
-        } 
+        }
 
-        int left = 0, right = 0;
-        int count = 0;
-        int minLeft = 0;
-        int minLength = Integer.MAX_VALUE;
-        
+        int left = 0, count = 0, minLeft = 0, minLength = Integer.MAX_VALUE;
 
-        while (right < s.length()) {
+        for (int right = 0; right < s.length(); right++) {
             char right_letter = s.charAt(right);
             if (t_map.containsKey(right_letter)) {
                 window.put(right_letter, window.getOrDefault(right_letter, 0) + 1);
@@ -26,23 +23,20 @@ class Solution {
                 }
             }
             while (count == t.length()) {
-                if (minLength > right - left + 1) {
+                if (right - left + 1 < minLength) {
                     minLength = right - left + 1;
                     minLeft = left;
                 }
-                //shrink the window by moving the left pointer
-                char left_letter = s.charAt(left);
+                // shrink window
+                char left_letter = s.charAt(left++);
                 if (window.containsKey(left_letter)) {
-                    window.put(left_letter, window.get(left_letter) - 1);
+                    window.put(left_letter, window.getOrDefault(left_letter, 0) - 1);
                     if (window.get(left_letter) < t_map.get(left_letter)) {
                         count--;
                     }
                 }
-                left++;
             }
-            right++;
         }
         return minLength == Integer.MAX_VALUE ? "" : s.substring(minLeft, minLeft + minLength);
-
     }
 }
