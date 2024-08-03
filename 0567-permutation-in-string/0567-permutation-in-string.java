0e98e10c99499fd1af06ef36f9a4d5391e583387
SX <shanxue2019@gmail.com>
2024-08-02 21:06:37 -0400

    Time: 959 ms (5.01%), Space: 45.3 MB (9.17%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,39 +1,26 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
         
-        HashMap<Character, Integer> window = new HashMap<>();
         HashMap<Character, Integer> s1_map = new HashMap<>();
-
+        HashMap<Character, Integer> window_map = new HashMap<>();
+        
         for (char letter : s1.toCharArray()) {
             s1_map.put(letter, s1_map.getOrDefault(letter, 0) + 1);
         }
-
-        int left = 0, right = 0, count = 0, minLength = Integer.MAX_VALUE;
-        while (right < s2.length()) {
-            char right_letter = s2.charAt(right);
-            if (s1_map.containsKey(right_letter)) {                
-                window.put(right_letter, window.getOrDefault(right_letter, 0) + 1);
-                if (window.get(right_letter) <= s1_map.get(right_letter)) {
-                    count++;
+        
+        for (int i = 0; i < s2.length() - s1.length() + 1; i++) {
+            for (int j = i; j < i + s1.length(); j++) {
+                char right_letter = s2.charAt(j);
+                if (s1_map.containsKey(right_letter)) {
+                    window_map.put(right_letter, window_map.getOrDefault(right_letter, 0) + 1);
                 }
             }
-            while (count == s1.length()) {
-                if (minLength > right - left + 1) {
-                    minLength = right - left + 1;
-                    if (minLength == s1.length()) {
-                        return true;
-                    }
-                }
-                char left_letter = s2.charAt(left);
-                if (window.containsKey(left_letter)) {
-                    window.put(left_letter, window.getOrDefault(left_letter, 0) - 1);
-                    if (window.get(left_letter) < s1_map.get(left_letter)) {
-                        count--;
-                    }
-                }
-                left++;
+            if (window_map.equals(s1_map)) {
+                return true;
             }
-            right++;
+            else {
+                window_map.clear();
+            }
         }
         return false;
     }
