2b460e2936c6be7563479291c03097a9b78784af
SX <shanxue2019@gmail.com>
2024-09-24 15:09:43 -0400

    Time: 0 ms (100.00%), Space: 41.5 MB (92.72%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -1,7 +1,10 @@
+// Using two for loops
+// Time complexity: O(n^2)
+// Space complexity: O(1)
 class Solution {
     public int removeElement(int[] nums, int val) {
-        int len = nums.length;
         
+        int len = nums.length;
         for (int i = 0; i < len; i++) {
             if (nums[i] == val) {
                 for (int j = i + 1; j < len; j++) {
@@ -11,6 +14,7 @@ class Solution {
                 len--;
             }
         }
+
         return len;
     }
 }
