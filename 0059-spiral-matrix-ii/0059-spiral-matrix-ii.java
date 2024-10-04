eb464bf4a5e2dffdb49f150165393e25cceb6847
SX <shanxue2019@gmail.com>
2024-10-03 21:33:53 -0400

    Time: 0 ms (100.00%), Space: 41.5 MB (18.50%) - LeetHub

--- 0059-spiral-matrix-ii/0059-spiral-matrix-ii.java
+++ 0059-spiral-matrix-ii/0059-spiral-matrix-ii.java
@@ -1,52 +1,42 @@
 class Solution {
     public int[][] generateMatrix(int n) {
+        
         int[][] result = new int[n][n];
         int startX = 0, startY = 0;
+        int count = 1;
         int loop = 1;
         int offset = 1;
         int i, j;
-        int val = 1;
 
-        while (loop <= n / 2 + 1) {
+        while (loop <= n / 2) {
             // top row
             for (j = startY; j < n - offset; j++) {
-                result[startX][j] = val;
-                val++;
-                System.out.print("(" + startX + "," + j + ")");
-
+                result[startX][j] = count;
+                count++;
             }
-            System.out.println();
-            // right column
+            // right column 
             for (i = startX; i < n - offset; i++) {
-                result[i][j] = val;
-                val++;
-                System.out.print("(" + i + "," + j + ")");
+                result[i][j] = count;
+                count++;
             }
-            System.out.println();
             // bottom row
-            for (; j > startY; j--) {
-                result[i][j] = val;
-                val++;
-                System.out.print("(" + i + "," + j + ")");
+            for (; j > startX; j--) {
+                result[i][j] = count;
+                count++;
             }
-            System.out.println();
-            // left column
-            for (; i > startX; i--) {
-                result[i][j] = val;
-                val++;
-                System.out.print("(" + i + "," + j + ")");
+            //left column
+            for (; i > startY; i--) {
+                result[i][j] = count;
+                count++;
             }
-
             startX++;
             startY++;
-            offset++;
             loop++;
+            offset++;
         }
-
         if (n % 2 == 1) {
-            result[n / 2][n / 2] = val;
+            result[startX][startY] = n * n;
         }
-        
         return result;
     }
 }
