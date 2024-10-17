2fdedecf90a8031e98be955387b65c7127b0ed30
SX <shanxue2019@gmail.com>
2024-10-16 23:45:35 -0400

    Time: 1 ms (66.19%), Space: 40.3 MB (92.70%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isHappy(int n) {
+        
+        Set<Integer> hashset = new HashSet<>();
+        
+        while (n != 1 && !hashset.contains(n)) {
+            hashset.add(n);
+            n = getNextNumber(n);
+        }
+        
+        return n == 1;
+    }
+    public int getNextNumber(int n) {
+        int result = 0;
+        while (n > 0) {
+            int temp = n % 10;
+            result += temp * temp;
+            n = n / 10;
+        }
+
+        return result;
+    }
+}
