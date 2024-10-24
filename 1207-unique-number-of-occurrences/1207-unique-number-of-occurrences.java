4ede9a648a84f1c10829d70b1470807dc7aae2c6
SX <shanxue2019@gmail.com>
2024-10-24 17:42:37 -0400

    Time: 2 ms (99.01%), Space: 42.9 MB (6.39%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.java
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.java
@@ -1,19 +1,20 @@
 class Solution {
     public boolean uniqueOccurrences(int[] arr) {
-        
-        Map<Integer, Integer> hashmap = new HashMap<>();
-        
+
+        int[] hashmap = new int[2001]; // Constraints: -1000 <= arr[i] <= 1000
         for (int i = 0; i < arr.length; i++) {
-            hashmap.put(arr[i], hashmap.getOrDefault(arr[i], 0) + 1);
+            int index = arr[i] + 1000; // convert to positive number, prevent negative index
+            hashmap[index]++;
         }
 
-        // start from 1, bucket stores number of occurrences of existing element
-        Set<Integer> hashset = new HashSet<>();
-        for (int i : hashmap.values()) {
-            if (hashset.contains(i)) {
+        int[] occurrence = new int[1001]; // Constraints: 1 <= arr.length <= 1000, means the max occurrence is 1000
+
+        for (int i = 0; i < hashmap.length; i++) {
+            int value = hashmap[i];
+            if (value > 0 && occurrence[value] > 0) {
                 return false;
             }
-            hashset.add(i);
+            occurrence[value]++; 
         }
         return true;
     }
