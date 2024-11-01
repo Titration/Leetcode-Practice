aa11fedcbdbefd1cc511591743612fa5f7e2200c
SX <shanxue2019@gmail.com>
2024-10-31 23:58:22 -0400

    Time: 2 ms (97.47%), Space: 41.3 MB (86.37%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -4,6 +4,7 @@ class Solution {
         Stack<Character> stack = new Stack<>();
 
         if (s.length() % 2 != 0) { return false; }
+        
         for (char c : s.toCharArray()) {
             if (c == '(') {
                 stack.push(')');
@@ -14,10 +15,7 @@ class Solution {
             else if (c == '[') {
                 stack.push(']');
             }
-            else if (stack.isEmpty()) {
-                return false;
-            }
-            else if (c != stack.peek()) {
+            else if (stack.isEmpty() || c != stack.peek()) {
                 return false;
             }
             else {
