02139431c4f5734e688309b97f66c25c38d0e052
SX <shanxue2019@gmail.com>
2024-11-13 11:36:07 -0500

    Time: 0 ms (100.00%), Space: 41 MB (83.43%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
@@ -3,8 +3,8 @@ class MyQueue {
     Stack<Integer> stack_out;
 
     public MyQueue() {
-        stack_in = new Stack<Integer>();
-        stack_out = new Stack<Integer>();
+        stack_in = new Stack<>();
+        stack_out = new Stack<>();
     }
     
     public void push(int x) {
@@ -12,28 +12,30 @@ class MyQueue {
     }
     
     public int pop() {
-        if (stack_out.isEmpty()) {
-            while (!stack_in.isEmpty()) {
-               stack_out.push(stack_in.pop());
-            }
-        }
+        dumpStackIn();
         return stack_out.pop();
     }
     
+    // public int peek() {
+    //     dumpStackIn();
+    //     return stack_out.peek();
+    // }
     public int peek() {
+        int removed_element = this.pop();
+        stack_out.push(removed_element);
+        return removed_element;
+    }
+    
+    public boolean empty() {
+        return (stack_in.isEmpty() && stack_out.isEmpty());
+    }
+
+    private void dumpStackIn() {
         if (stack_out.isEmpty()) {
             while (!stack_in.isEmpty()) {
                 stack_out.push(stack_in.pop());
             }
         }
-        return stack_out.peek();
-    }
-    
-    public boolean empty() {
-        if (stack_in.isEmpty() && stack_out.isEmpty()) {
-            return true;
-        }
-        return false;
     }
 }
 
