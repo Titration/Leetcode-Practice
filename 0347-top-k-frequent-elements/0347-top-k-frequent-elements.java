e75091173cae9d7bcd8a3d2d32f862581ff5c1ea
SX <shanxue2019@gmail.com>
2024-11-25 14:50:35 -0500

    Time: 13 ms (76.16%), Space: 48.8 MB (33.45%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -2,31 +2,30 @@ class Solution {
     public int[] topKFrequent(int[] nums, int k) {
         
         Map<Integer, Integer> hashmap = new HashMap<>();
-        ArrayList<Integer>[] array = new ArrayList[nums.length + 1];
-        int count = 0;
+        // Min heap
+        Queue<int[]> pq = new PriorityQueue<>((pair1, pair2) -> pair1[1] - pair2[1]);
         int[] result = new int[k];
 
         for (int key : nums) {
             hashmap.put(key, hashmap.getOrDefault(key, 0) + 1);
         }
-        
-        for (int key : hashmap.keySet()) {
-            int value = hashmap.get(key);
-            if (array[value] == null) {
-                array[value] = new ArrayList<>();
-            }
-            array[value].add(key);
-        }
 
-        for (int i = array.length - 1; i >= 0; i--) {
-            if (array[i] != null) {
-                for (int j = 0; j < array[i].size() && count < k; j++) {
-                    result[count] = array[i].get(j);
-                    count++;
+        for (Map.Entry<Integer, Integer> entry : hashmap.entrySet()) {
+            if (pq.size() < k) {
+                pq.add(new int[] {entry.getKey(), entry.getValue()});
+            }
+            else {
+                if (entry.getValue() > pq.peek()[1]) {
+                    pq.poll();
+                    pq.add(new int[] {entry.getKey(), entry.getValue()});
                 }
             }
-            
         }
+
+        for (int i = 0; i < k; i++) {
+            result[i] = pq.poll()[0];
+        }
+
         return result;
     }
 }
